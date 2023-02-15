b1eee0b4e444263dcffa95aa22381adb8d322824
Specter-0 <sigurd.thornes@gmail.com>
2023-02-15 10:53:18 +0100

    dwoorr but it

--- pygameTests.py
+++ pygameTests.py
@@ -3,16 +3,36 @@ import pgu
 pg.init()
 pgu.init()
 
-screen = pg.display.set_mode([500, 500])
+screen = pg.display.set_mode([800, 800])
 
-class Player(pg.sprite.Sprite, pgu.utility.sprite.movement):
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pg.Surface((25, 25))
-        self.surf.fill((0, 0, 0))
+class Planet(pg.sprite.Sprite, pgu.utility.sprite.movement):
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 250 / AU # 1AU = 100px
+    TIMESTEP = 3600*24 # 1 day
+
+    def __init__(self, x, y, radius, color, mass):
+        super(Planet, self).__init__()
+        self.x = x
+        self.y = y 
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
+        self.y_vel = 0
+        self.x_vel = 0
+
+
+
+
         self.rect = self.surf.get_rect()
 
-player = Player()
+
+player = Planet()
 
 def main(running):
     clock = pg.time.Clock()
@@ -22,7 +42,7 @@ def main(running):
             if event.type == pg.QUIT:
                 running = False
 
-        screen.fill((255, 255, 255))
+        screen.fill((pgu.WHITE))
 
         screen.blit(player.surf, player.rect)
 
