6ec24eec2c9238953e2c7cbf33b6c6562858d18c
Specter-0 <sigurd.thornes@gmail.com>
2023-02-15 13:38:47 +0100

    made some small changes

--- pygameTests.py
+++ pygameTests.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import pgu 
+import math
 pg.init()
 pgu.init()
 
@@ -31,22 +32,60 @@ class Planet(pg.sprite.Sprite, pgu.utility.sprite.movement):
     
     def draw(self, win):
         x = self.x * self.SCALE + width / 2
-        y = self.x * self.SCALE + height / 2
+        y = self.y * self.SCALE + height / 2
         pg.draw.circle(win, self.color, (x, y), self.radius)
+
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+        
+        force = self.G * self.mass * other.mass /distance**2
+        v = math.atan2(distance_y, distance_x)
+        force_x = math.cos(v) * force
+        force_y = math.sin(v) * force
+        return force_x, force_y
+    
+    def update_pos(self, planets):
+        total_fx = total_fy = 0
+
+        for planet in planets:
+            if self == planet:
+                continue
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
+
 
 def main(running):
     clock = pg.time.Clock()
 
-    sun = Planet(0, 0, 30, pgu.YELLOW, 1.98892 * 10**30, )
+    sun = Planet(0, 0, 30, pgu.YELLOW, 1.98892 * 10**30)
     sun.sun = True
 
     earth = Planet(-1 * Planet.AU, 0, 16, pgu.BLUE, 5.9742 * 10*24)
+    earth.y_vel = 29.783 * 1000
 
     mars = Planet(-1.524 * Planet.AU, 0, 12, pgu.RED, 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
 
     mercury = Planet(-0.387 * Planet.AU, 0, 8, pgu.GRAY, 0.330 * 10**24)
+    mercury.y_vel = 47.4 * 1000
 
     venus = Planet(-0.723 * Planet.AU, 0, 14, pgu.WHITE, 4.8685 * 10**24)
+    venus.y_vel = 35.02 * 1000
 
     planets = [sun, earth, mars, mercury, venus]
 
@@ -59,6 +98,7 @@ def main(running):
         win.fill((pgu.BLACK))
 
         for planet in planets:
+            planet.update_pos(planets)
             planet.draw(win)
 
         pg.display.flip()   
